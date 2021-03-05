e118938406a5dfa142dbf2d9484899f9ade18bc3
glenbuck503 <glenbuck503@gmail.com>
2021-03-05 10:40:47 -0800

    add clearall

--- VendorAndOrder/Models/Vendor.cs
+++ VendorAndOrder/Models/Vendor.cs
@@ -6,12 +6,15 @@ namespace VendorAndOrder.Models
   {
     private static List<Vendor> _instances = new List<Vendor> {};
     public string Name { get; set;}
+    public string Description { get; set;}
     public int Id { get;}
     public List<Vendor> Vendors { get; set;}
 
-    public Vendor(string vendorName)
+    public Vendor(string vendorName, string vendorDescription)
     {
       Name = vendorName;
+      Description = vendorDescription;
+
       _instances.Add(this);
       Id = _instances.Count;
       Vendors = new List<Vendor>{};

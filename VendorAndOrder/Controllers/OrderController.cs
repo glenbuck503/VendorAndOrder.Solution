5f59fe2b767423ef0c5a07cf98480b2d2e4f6840
glenbuck503 <glenbuck503@gmail.com>
2021-03-05 13:52:27 -0800

    added New route to OrderController

--- VendorAndOrder/Controllers/OrderController.cs
+++ VendorAndOrder/Controllers/OrderController.cs
@@ -6,7 +6,13 @@ namespace VendorAndOrder.Controllers
 {
   public class OrdersController : Controller
   {
-    
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+
 
   }
 }

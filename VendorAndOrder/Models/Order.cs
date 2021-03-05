687a6a89a79b8e9f96ff9557292aed04a2c9ce64
glenbuck503 <glenbuck503@gmail.com>
2021-03-05 08:18:33 -0800

    add properties to Order class

---
+++ VendorAndOrder/Models/Order.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace VendorAndOrder.Models
+{
+  public class Order
+  {
+    public string OrderName { get; set; }
+    public string Description { get; set; }
+    public string Price { get; set; }
+    public string Date { get; set; }
+
+
+  }
+}

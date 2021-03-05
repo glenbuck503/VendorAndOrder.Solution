dde9174df2d336a99ab330abb2e21089a70356d7
glenbuck503 <glenbuck503@gmail.com>
2021-03-05 10:08:31 -0800

    add Vendor constructor

--- VendorAndOrder/Models/Vendor.cs
+++ VendorAndOrder/Models/Vendor.cs
@@ -4,7 +4,23 @@ namespace VendorAndOrder.Models
 {
   public class Vendor 
   {
-    
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set;}
+    public int Id { get;}
+    public List<Vendor> Vendors { get; set;}
+
+    public Vendor(string vendorName)
+    {
+      Name = vendorName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Vendors = new List<Vendor>{};
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
 
   }
 }

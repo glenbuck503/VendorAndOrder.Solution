ee8685229db9c0063f00ac5f624c895031c19b1a
glenbuck503 <glenbuck503@gmail.com>
2021-03-05 12:17:41 -0800

    bad test for Find_ReturnsCorrectCategory_Category()

--- VendorAndOrder.Tests/ModelTests/VendorTests.cs
+++ VendorAndOrder.Tests/ModelTests/VendorTests.cs
@@ -60,8 +60,8 @@ namespace ToDoList.Tests
     public void GetAll_ReturnsAllVendorObjects_VendorList()
     {
       //Arrange
-      string vendor1 = "Work";
-      string vendor2 = "School";
+      string vendor1 = "Vendor1";
+      string vendor2 = "Vendor2";
       Vendor newVendor1 = new Vendor(vendor1, "sdfsd");
       Vendor newVendor2 = new Vendor(vendor2, "sdfsds");
       List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
@@ -71,6 +71,22 @@ namespace ToDoList.Tests
 
       //Assert
       CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectCategory_Category()
+    {
+      //Arrange
+      string vendor1 = "Vendor1";
+      string vendor2 = "Vendor2";
+      Vendor newVendor1 = new Vendor(vendor1, "sdfsd");
+      Vendor newVendor2 = new Vendor(vendor2, "232");
+
+      //Act
+      Vendor result = Vendor.Find(3);
+
+      //Assert
+      Assert.AreEqual(newVendor2, result);
     }
     
 

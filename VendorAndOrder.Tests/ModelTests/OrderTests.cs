44db60ad0ef555f32752669ab5a75f99189609bf
glenbuck503 <glenbuck503@gmail.com>
2021-03-05 08:45:34 -0800

    bad test for Order instance

---
+++ VendorAndOrder.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorAndOrder.Models;
+using System;
+
+namespace VendorAndOrder.Tests
+{
+  [TestClass]
+  public class ItemTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test", "test2", "test3");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
+
+    

1dc20e822bd52f4c6a85498296124743138700de
glenbuck503 <glenbuck503@gmail.com>
2021-03-05 13:34:38 -0800

    looking over routes to figure out how to apporach and then 10 minute break

---
+++ VendorAndOrder/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using VendorAndOrder.Models;
+using System.Collections.Generic;
+
+namespace VendorAndOrder.Controllers
+{
+  public class OrdersController : Controller
+  {
+    
+
+  }
+}
